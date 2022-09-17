ad7114955e2a1c61496953ad376f22a6b8040726
Ariel-Marcelo <ariel.pillajo@epn.edu.ec>
2022-09-17 17:39:57 -0500

    feat: add items to shopping cart

--- components/AddToCart/index.tsx
+++ components/AddToCart/index.tsx
@@ -8,17 +8,24 @@ type AppToCartProps = {
 
 const AddToCart = ({id}: AppToCartProps): JSX.Element => {
   const [ counter, setCounter ] = React.useState(1);
-  const { dispatch } = React.useContext(GlobalCountContex);
+  const { state, dispatch } = React.useContext(GlobalCountContex);
 
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setCounter( Number(event.target.value) );
   }
+  const onSubmit = () => {
+    dispatch({type: Types.ADD_FOR_SALE, payload: {count: counter}})
+    dispatch({ type: Types.ADD_SHOPPING_CART, payload: { id: id, count: counter } });
+  }
+  const print = () => {
+    console.log(state);
+  }
+
   return (
     <div className='add-to-cart'>
       <input type="number" min="1" onChange={onChange} value={counter}/> 
-      <button onClick={() => { 
-        dispatch({type: Types.ADD_FOR_SALE, payload: {count: counter}})
-      }}> Add to Cart </button>
+      <button onClick={onSubmit}> Add to Cart </button>
+      <button onClick={print}> STATE </button>
     </div>
   );
 }
--- components/reducers.ts
+++ components/reducers.ts
@@ -9,19 +9,32 @@ const productReducer = (
   state: ProductType[], 
   action: ProductActions | ShoppingCartActions
   ) => {
+  console.log("This state is product: ",state);
   switch (action.type) {
     case "ADD_SHOPPING_CART":
-      return [
-        ...state,
-        {
-          id: action.payload.id,
-          count: action.payload.count,
-        },
-      ];
+      console.log("payload for add shopping cart",action.payload.count);
+      if ( state?.find((item)=> item.id == action.payload.id)) {
+        return (
+          state.map( 
+            (item) => { 
+              if (item.id == action.payload.id) {
+                item.count += action.payload.count; 
+              } 
+               return item; 
+            }
+          )
+        );
+      } else {
+        const newItem = { id: action.payload.id, count: action.payload.count };
+        return [...state, newItem];
+      }
     case "REMOVE_SHOPPING_CART":
       return [
         ...state.filter((item) => item.id !== action.payload.id),
       ];
+
+    default:
+      return state;
   }
 };
 
@@ -29,11 +42,14 @@ const forSaleReducer = (
   state: number, 
   action: ProductActions | ShoppingCartActions
   ) => {
+  console.log("This state is for sale: ",state);
   switch (action.type) {
     case "ADD_FOR_SALE":
       return state + action.payload.count;
     case "REMOVE_FOR_SALE":
       return state - 1;
+    default: 
+      return state;
   }
 }
 
